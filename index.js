5f1c70a3c68498b26c46b479db7abf61f17038c3
shubhamkmit9022 <shubham.r@tessrac.com>
2024-01-19 17:32:17 +0530

    data updated of json server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
+
+// const jsonServer = require("json-server"); // importing json-server library
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
