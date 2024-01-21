6554e1ddf8b3e9ee2a2bef23bcdd6579def0a4ae
shubhamkmit9022 <shubham.r@tessrac.com>
2024-01-21 19:44:17 +0530

    code updated of index.js file and json-server version issue

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const jsonServer = require('json-server');
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000
 
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-
-server.use(cors());
-server.use(middlewares);
-server.use(router);
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
-
-
-// const jsonServer = require("json-server"); // importing json-server library
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+  console.log('JSON Server is running on ',port)
+})
